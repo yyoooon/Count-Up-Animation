82f9016d9591dcd20bb93bdcaacfe01fbc5bc41d
yoon <yunyun40550082@gmail.com>
2022-07-03 03:56:25 +0900

    feat: 숫자 증가 애니메이션 컴포넌트 및 로직 구현

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,19 @@
-const HomePage = () => <h1>hi</h1>
+import styled from '@emotion/styled'
+
+import { CountUpText } from '@/components'
+
+const Wrapper = styled.div`
+  border: 1px solid gray;
+  max-width: 1200px;
+  margin: 0 auto;
+  height: 100vh;
+`
+const HomePage = () => (
+  <Wrapper>
+    <CountUpText maxNumber={350} unit="만 명" target="여행자" />
+    <CountUpText maxNumber={100} unit="만 개" target="여행 리뷰" />
+    <CountUpText maxNumber={470} unit="만 개" target="여행 일정" />
+  </Wrapper>
+)
 
 export default HomePage
