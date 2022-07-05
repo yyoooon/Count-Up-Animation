fe81e354cc153f25f12a83ec933f96132acf44c0
yoon <yunyun40550082@gmail.com>
2022-07-05 19:22:04 +0900

    refactor: logoImage컴포넌트를 BackgroundImage컴포넌트와 텍스트의 조합으로 수정

---
+++ src/components/atoms/BackgroundImage/BackgroundImage.tsx
@@ -0,0 +1,44 @@
+import styled from '@emotion/styled'
+
+import { triple } from '@/assets'
+
+interface BackgroundImageType extends React.ComponentProps<'div'> {
+  imageUrl: string
+  width: string | number
+  height: string | number
+  children?: React.ReactElement | React.ReactElement[]
+}
+
+const BackgroundImageBox = styled.div<{
+  imageUrl: string
+  width: string | number
+  height: string | number
+}>`
+  width: ${({ width }) => (typeof width === 'number' ? `${width}px` : width)};
+  height: ${({ height }) =>
+    typeof height === 'number' ? `${height}px` : height};
+  text-align: center;
+  background-image: ${({ imageUrl }) => `url(${imageUrl})`};
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+`
+
+const BackgroundImage = ({
+  children,
+  imageUrl,
+  width = '100px',
+  height = '100px',
+  ...props
+}: BackgroundImageType) => (
+  <BackgroundImageBox
+    imageUrl={triple}
+    width={width}
+    height={height}
+    {...props}
+  >
+    {children}
+  </BackgroundImageBox>
+)
+
+export default BackgroundImage
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,9 @@
 export { default as MetricText } from './atoms/MetricText/MetricText'
 export { default as AwardText } from './atoms/AwardText/AwardText'
-export { default as LogoImage } from './atoms/LogoImage/LogoImage'
+export { default as BackgroundImage } from './atoms/BackgroundImage/BackgroundImage'
 
 export { default as AwardTextGroup } from './molecules/AwardTextGroup/AwardTextGroup'
 export { default as MetricTextGroup } from './molecules/MetricTextGroup/MetricTextGroup'
+export { default as LogoImage } from './molecules/LogoImage/LogoImage'
 
 export { default as AchievementSection } from './organisms/AchievementSection/AchievementSection'
